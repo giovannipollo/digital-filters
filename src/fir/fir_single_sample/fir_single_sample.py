Fixed single sample filters due to a wrong right shift rotation

src/fir/fir_single_sample/fir_single_sample.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class FIRSingleSample:
     def __init__(self, filter_order: int, coefficients: np.ndarray):
         """
@@ -16,11 +17,11 @@ class FIRSingleSample:
         self.number_of_taps = filter_order + 1
         self.coefficients = coefficients
         self.input_buffer = np.zeros(filter_order)
-    
+
     def apply_fir_filter(self, x: float) -> float:
         """
         Apply FIR filter to input signal.
-        
+
         Args:
             x (float): Input sample
 
@@ -29,9 +30,13 @@ class FIRSingleSample:
         """
         out_filtered = x * self.coefficients[0]
         for i in range(1, self.number_of_taps):
-            out_filtered = out_filtered + self.input_buffer[i - 1] * self.coefficients[i]
-        for i in range(1, self.number_of_taps -1):
-            self.input_buffer[i] = self.input_buffer[i - 1]
+            out_filtered = (
+                out_filtered + self.input_buffer[i - 1] * self.coefficients[i]
+            )
+
+        # Rotate the input buffer to the right of one position
+        for i in range(1, self.number_of_taps - 1):
+            self.input_buffer[self.number_of_taps - 1 - i] = self.input_buffer[
+                self.number_of_taps - 2 - i
+            ]
         self.input_buffer[0] = x
-
-

src/iir/iir_array/iir_array.py
@@ -5,8 +5,7 @@ class IIRArray:
         self.b = b
         self.a = a
 
-    @staticmethod
-    def apply_iir_filter(x, b, a):
+    def apply_iir_filter(self, x):
         """
         Apply IIR filter to input signal.
         
@@ -20,8 +19,8 @@ class IIRArray:
         """
         # Ensure input is a numpy array
         x = np.asarray(x)
-        b = np.asarray(b)
-        a = np.asarray(a)
+        b = np.asarray(self.b)
+        a = np.asarray(self.a)
         
         if x.ndim != 1 or b.ndim != 1 or a.ndim != 1:
             raise ValueError("Input signal, numerator and denominator coefficients must be 1D arrays")
@@ -34,9 +33,9 @@ class IIRArray:
         for n in range(input_signal_length):
             for k in range(taps):
                 if n - k >= 0:
-                    y[n] += b[k] * x[n - k]
+                    y[n] = y[n] + b[k] * x[n - k]
             for k in range(1, taps):
                 if n - k >= 0:
-                    y[n] -= a[k] * y[n - k]
+                    y[n] = y[n] - a[k] * y[n - k]
             y[n] = y[n] / a[0]
         return y

src/iir/iir_single_sample/iir_single_sample.py
@@ -3,6 +3,23 @@ import numpy as np
 
 class IIRSingleSample:
     def __init__(self, b: np.ndarray, a: np.ndarray, filter_order: int):
+        """
+        Initialize IIR filter.
+
+        Args:
+            b (np.ndarray): Numerator coefficients
+            a (np.ndarray): Denominator coefficients
+            filter_order (int): Filter order 
+
+        Returns:
+            None
+
+        Raises:
+            ValueError: If the length of the numerator coefficients (b) is not equal to the filter order + 1
+            ValueError: If the length of the denominator coefficients (a) is not equal to the filter order + 1
+            ValueError: If the length of the numerator and denominator coefficients (b and a respectively) are not equal
+
+        """
         self.b = b
         self.a = a
 
@@ -12,7 +29,8 @@ class IIRSingleSample:
             raise ValueError("The length of the denominator coefficients (a) must be equal to the filter order + 1")
         if len(b) != len(a):
             raise ValueError("The length of the numerator and denominator coefficients (b and a respectively) must be equal")
-
+        
+        self.num_taps = filter_order + 1
         self.input_buffer = np.zeros(filter_order)
         self.output_buffer = np.zeros(filter_order)
 
@@ -30,11 +48,15 @@ class IIRSingleSample:
         for i in range(1, len(self.b)):
             y = y + self.b[i] * self.input_buffer[i - 1]
         for i in range(1, len(self.a)):
-            y = y + self.a[i] * self.output_buffer[i - 1]
-        for i in range(1, len(self.b)):
-            self.input_buffer[i] = self.input_buffer[i - 1]
+            y = y - self.a[i] * self.output_buffer[i - 1]
+        
+        # Rotate the input buffer to the right of one position
+        for i in range(1, len(self.b) - 1):
+            self.input_buffer[self.num_taps - 1 - i] = self.input_buffer[self.num_taps - 2 - i]
         self.input_buffer[0] = x
-        for i in range(1, len(self.a)):
-            self.output_buffer[i] = self.output_buffer[i - 1]
+
+        # Rotate the output buffer to the right of one position
+        for i in range(1, len(self.a) - 1):
+            self.output_buffer[self.num_taps - 1 - i] = self.output_buffer[self.num_taps - 2 -i]
         self.output_buffer[0] = y
         return y

src/iir/test/test_iir_array.py
@@ -5,9 +5,11 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import pytest
 import numpy as np
 import yaml
-from iir_array.iir_array import IIRArray
 from scipy import signal
-def test_apply_iir_filter():
+from iir_array.iir_array import IIRArray
+from iir_single_sample.iir_single_sample import IIRSingleSample
+
+def test_apply_iir_filter_array():
     # Load input signal
     with open("src/iir/test/input_signal.txt", "rb") as f:
         input_signal = np.loadtxt(f)
@@ -20,7 +22,36 @@ def test_apply_iir_filter():
 
     # Create IIRArray instance and apply filter
     iir_array = IIRArray(b=b, a=a)
-    y = iir_array.apply_iir_filter(x=input_signal, b=b, a=a)
+    y = iir_array.apply_iir_filter(x=input_signal)
+
+    # Compute the expected output signal with scipy
+    y_scipy = signal.lfilter(b=b, a=a, x=input_signal)
+
+    # Compute the mean squared error
+    mse = np.mean((y - y_scipy) ** 2)
+    mae = np.mean(np.abs(y - y_scipy))
+    max_abs_diff = np.max(np.abs(y - y_scipy))
+
+    assert mse < 1e-10
+    assert mae < 1e-5
+    assert max_abs_diff < 1e-5
+
+def test_apply_iir_filter_single_sample():
+    # Load input signal
+    with open("src/iir/test/input_signal.txt", "rb") as f:
+        input_signal = np.loadtxt(f)
+
+    # Load coefficients
+    with open("src/iir/test/coefficient.yaml") as f:
+        coefficient = yaml.safe_load(f)
+    b = coefficient["b"]
+    a = coefficient["a"]
+
+    # Create IIRSingleSample instance and apply filter
+    iir_single_sample = IIRSingleSample(b=b, a=a, filter_order=len(b) - 1)
+    y = np.zeros_like(input_signal)
+    for i in range(len(input_signal)):
+        y[i] = iir_single_sample.apply_iir_filter(x=input_signal[i])
 
     # Compute the expected output signal with scipy
     y_scipy = signal.lfilter(b=b, a=a, x=input_signal)